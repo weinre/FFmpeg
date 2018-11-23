f530f07224a8551bbaa3aa487c04e9ab2a1f5c66
whenSunset <a1018998632@gmail.com>
2018-11-23 12:29:53 +0800

    build succeed

---
+++ build_android.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+cd /Users/whensunset/AndroidStudioProjects/KSVideoProject/ffmpeg
+# make clean
+# NDK的路径，根据自己的安装位置进行设置
+export NDK=/Users/whensunset/AndroidStudioProjects/KSVideoProject/android-ndk-r14b
+export SYSROOT=$NDK/platforms/android-16/arch-arm/
+export TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64
+export CPU=arm
+export PREFIX=$(pwd)/android/$CPU
+export ADDI_CFLAGS="-marm"
+function build_one
+{
+./configure \
+    --prefix=$PREFIX \
+    --target-os=linux \
+    --cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
+    --arch=arm \
+    --sysroot=$SYSROOT \
+    --extra-cflags="-Os -fpic $ADDI_CFLAGS" \
+    --extra-ldflags="$ADDI_LDFLAGS" \
+    --cc=$TOOLCHAIN/bin/arm-linux-androideabi-gcc \
+    --nm=$TOOLCHAIN/bin/arm-linux-androideabi-nm \
+    --enable-shared \
+    --enable-runtime-cpudetect \
+    --enable-gpl \
+    --enable-small \
+    --enable-cross-compile \
+    --disable-debug \
+    --disable-static \
+    --disable-doc \
+    --disable-asm \
+    --disable-ffmpeg \
+    --disable-ffplay \
+    --disable-ffprobe \
+    --disable-ffserver \
+    --enable-postproc \
+    --enable-avdevice \
+    --disable-symver \
+    --disable-stripping \
+$ADDITIONAL_CONFIGURE_FLAG
+sed -i '' 's/HAVE_LRINT 0/HAVE_LRINT 1/g' config.h
+sed -i '' 's/HAVE_LRINTF 0/HAVE_LRINTF 1/g' config.h
+sed -i '' 's/HAVE_ROUND 0/HAVE_ROUND 1/g' config.h
+sed -i '' 's/HAVE_ROUNDF 0/HAVE_ROUNDF 1/g' config.h
+sed -i '' 's/HAVE_TRUNC 0/HAVE_TRUNC 1/g' config.h
+sed -i '' 's/HAVE_TRUNCF 0/HAVE_TRUNCF 1/g' config.h
+sed -i '' 's/HAVE_CBRT 0/HAVE_CBRT 1/g' config.h
+sed -i '' 's/HAVE_RINT 0/HAVE_RINT 1/g' config.h
+make clean
+make -j8
+make install
+}
+build_one
